a2d28d610e2d69a242a56ac64c8588c843fb581e
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-09-17 17:08:46 -0300

    Made Who-am-i and NavBar components available to the user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,7 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'aboutus', component: WhoAmIComponent }
     
+    
 
 
     //{ path: '**', component: PageNotFoundComponent },
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { ChatComponent } from '../chat/chat.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,ChatComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
