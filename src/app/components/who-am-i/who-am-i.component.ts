d5bbb49b9dd400e3e580ad8d16ff6f07f27f86a1
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-09-17 18:12:51 -0300

    Added WhoAmI img

--- src/app/components/who-am-i/who-am-i.component.ts
+++ src/app/components/who-am-i/who-am-i.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './who-am-i.component.css'
 })
 export class WhoAmIComponent {
-
+  imageUrl: string = 'https://firebasestorage.googleapis.com/v0/b/labo4-4bb6e.appspot.com/o/me.jpeg?alt=media&token=82d7cc03-a635-4235-958c-8b4bb8d28bae';
 }
