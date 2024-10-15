b5ff476b776bd45da6d0f8a9aa4fac69735b4279
AleFalcone27 <alejofalocne60@gamil.com>
2024-10-15 16:15:17 -0300

    Dialog component init

---
+++ src/app/components/dialog/dialog.component.ts
@@ -0,0 +1,42 @@
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './dialog.component.html',
+  styleUrl: './dialog.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
+})
+export class DialogComponent {
+  readonly dialog = inject(MatDialog);
+
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(DialogAnimations, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  templateUrl: 'dialog-animations-example-dialog.html',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogAnimations {
+  readonly dialogRef = inject(MatDialogRef<DialogAnimations>);
+}
