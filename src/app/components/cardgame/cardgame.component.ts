ec93ddceea6daeed46d19d877f73ca1a8c590f6b
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-10-16 20:16:45 -0300

    Added method endGame

--- src/app/components/cardgame/cardgame.component.ts
+++ src/app/components/cardgame/cardgame.component.ts
@@ -11,10 +11,10 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-cardgame',
   standalone: true,
-  imports: [SnackBarOverviewExample,CommonModule,DialogComponent],
+  imports: [SnackBarOverviewExample, CommonModule, DialogComponent],
   templateUrl: './cardgame.component.html',
   styleUrl: './cardgame.component.css',
-  providers :[SnackBarOverviewExample,DialogComponent]
+  providers: [SnackBarOverviewExample, DialogComponent]
 
 })
 export class CardgameComponent implements OnInit {
@@ -23,31 +23,19 @@ export class CardgameComponent implements OnInit {
   guessCount: number = 0;
   score: number = 0;
 
-  constructor(private router:Router, private scoreService:ScoreService, private cardsApiService: CardsApiService,private snackBar: SnackBarOverviewExample, private dialog:MatDialog, private dialogComponent:DialogComponent) { }
+  constructor(private router: Router, private scoreService: ScoreService, private cardsApiService: CardsApiService, private snackBar: SnackBarOverviewExample, private dialog: MatDialog, private dialogComponent: DialogComponent) { }
 
   async ngOnInit(): Promise<void> {
     this.cardsApiService.initDeck().subscribe((response) => {
       if (response && response.cards) {
         this.card = response.cards[0];
-      } 
+      }
     });
   }
 
   drawNext(guess: string): void {
     if (this.guessCount >= 10) {
-
-      console.log(this.snackBar.openSnackBar('Ya alcanzaste el máximo de intentos', '❌'));
-
-      this.dialogComponent.openDialog().afterClosed().subscribe(result => {
-
-        this.scoreService.WriteScore(1,this.score) // Guardamos la putuacion en la base de datos
-        
-        if (result) {
-          this.replay();
-        } else {
-          this.exit();
-        }
-      });
+      this.endgame()
     }
 
     this.lastCard = { ...this.card };
@@ -63,7 +51,7 @@ export class CardgameComponent implements OnInit {
         } else if (guess === 'menor' && newCardValue < lastCardValue) {
           this.score++;
         } else {
-          if (this.score > 0 ){
+          if (this.score > 0) {
             this.score--;
           }
         }
@@ -86,6 +74,21 @@ export class CardgameComponent implements OnInit {
       default:
         return parseInt(value);
     }
+  }
+
+  endgame() {
+    console.log(this.snackBar.openSnackBar('Ya alcanzaste el máximo de intentos', '❌'));
+
+    this.dialogComponent.openDialog().afterClosed().subscribe(result => {
+
+      this.scoreService.WriteScore(1, this.score) // Guardamos la putuacion en la base de datos
+
+      if (result) {
+        this.replay();
+      } else {
+        this.exit();
+      }
+    });
   }
 
   replay() {
