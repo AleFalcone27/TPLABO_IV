2b326d308546db1b3af90990ed20635d10d0f29f
AleFalcone27 <alejofalocne60@gamil.com>
2024-10-15 21:46:51 -0300

    Progress on keeping player score

--- src/app/components/cardgame/cardgame.component.ts
+++ src/app/components/cardgame/cardgame.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CardsApiService } from '../../services/cards-api/cards-api.service';
+import { MatDialog } from '@angular/material/dialog';
 import { tap } from 'rxjs/operators';
 import { SnackBarOverviewExample } from '../snack-bar/snack-bar.component';
 import { CommonModule } from '@angular/common';
-
-
+import { DialogComponent } from '../dialog/dialog.component';
+import { ScoreService } from '../../services/scores/score.service';
+import { Firestore, collection, addDoc, Timestamp } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-cardgame',
   standalone: true,
-  imports: [SnackBarOverviewExample,CommonModule],
+  imports: [SnackBarOverviewExample,CommonModule,DialogComponent],
   templateUrl: './cardgame.component.html',
   styleUrl: './cardgame.component.css',
-  providers :[SnackBarOverviewExample]
+  providers :[SnackBarOverviewExample,DialogComponent]
 
 })
 export class CardgameComponent implements OnInit {
@@ -21,7 +23,7 @@ export class CardgameComponent implements OnInit {
   guessCount: number = 0;
   score: number = 0;
 
-  constructor(private cardsApiService: CardsApiService,private snackBar: SnackBarOverviewExample) { }
+  constructor(private firestore:Firestore, private scoreService:ScoreService, private cardsApiService: CardsApiService,private snackBar: SnackBarOverviewExample, private dialog:MatDialog, private dialogComponent:DialogComponent) { }
 
   async ngOnInit(): Promise<void> {
     this.cardsApiService.initDeck().subscribe((response) => {
@@ -33,8 +35,21 @@ export class CardgameComponent implements OnInit {
 
   drawNext(guess: string): void {
     if (this.guessCount >= 10) {
-      this.snackBar.openSnackBar('Ya alcanzaste el máximo de intentos', '❌');
-      return;
+      console.log(this.snackBar.openSnackBar('Ya alcanzaste el máximo de intentos', '❌'));
+
+      this.dialogComponent.openDialog().afterClosed().subscribe(result => {
+        if (result) {
+          // Logica para volver a jugar
+          // llamar funcion replay
+
+
+          console.log('User  wants to play again');
+        } else {
+          // Guardar el puntaje
+          // Logica para salir del juego a home
+          console.log('User  wants to exit');
+        }
+      });
     }
 
     this.lastCard = { ...this.card };
@@ -74,6 +89,13 @@ export class CardgameComponent implements OnInit {
         return parseInt(value);
     }
   }
+  async WriteLog() {
+    let col = collection(this.firestore, 'Scores');
+    const docRef = await addDoc(col, {
+      // add controller properties
+    });
+  }
+
 }
 
 
