0d631d9b73641430cc73638c250fcab3bf95aa52
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-10-16 20:25:20 -0300

    Now using AuthService to get current user email

--- src/app/services/scores/score.service.ts
+++ src/app/services/scores/score.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
 import { user } from '@angular/fire/auth';
 import { Firestore, collection, addDoc, Timestamp, snapToData } from '@angular/fire/firestore';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScoreService {
 
-  constructor(private firestore: Firestore) { }
+  constructor(private firestore: Firestore, private authService:AuthService) { }
 
-  async WriteScore(gameID:number, score:number, userEmail:string) {
+  async WriteScore(gameID:number, score:number) {
+    let currentUserEmail = this.authService.getCurrentUserEmail();
+    this.authService.getCurrentUSername();
     let col = collection(this.firestore, 'Scores');
     const docRef = await addDoc(col, {
       Date: Timestamp.now(),
       GameId: gameID,
       Score: score,
-      userEmail: userEmail      
+      userEmail: currentUserEmail       
     });
   }
 }
