af7a52dc0250fc2ce784c68ef7a4d75ca69ecc39
AleFalcone27 <alejofalocne60@gamil.com>
2024-10-21 20:38:51 -0300

    Added functionalities for score table

--- src/app/services/scores/score.service.ts
+++ src/app/services/scores/score.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { user } from '@angular/fire/auth';
-import { Firestore, collection, addDoc, Timestamp, snapToData } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, Timestamp, query, where, getDocs, orderBy } from '@angular/fire/firestore';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable({
@@ -8,9 +7,9 @@ import { AuthService } from '../auth/auth.service';
 })
 export class ScoreService {
 
-  constructor(private firestore: Firestore, private authService:AuthService) { }
+  constructor(private firestore: Firestore, private authService: AuthService) { }
 
-  async WriteScore(gameID:number, score:number) {
+  async WriteScore(gameID: number, score: number) {
     let currentUserEmail = this.authService.getCurrentUserEmail();
     this.authService.getCurrentUSername();
     let col = collection(this.firestore, 'Scores');
@@ -18,7 +17,58 @@ export class ScoreService {
       Date: Timestamp.now(),
       GameId: gameID,
       Score: score,
-      userEmail: currentUserEmail       
+      userEmail: currentUserEmail
     });
   }
+
+  async GetScoresByGameID(gameID: number) {
+    let scoresCollection = collection(this.firestore, 'Scores');
+    let scoresQuery = query(
+      scoresCollection,
+      where('GameId', '==', gameID),
+      orderBy('Score', 'desc') 
+  );
+    let querySnapshot = await getDocs(scoresQuery);
+    let scores: { userEmail: string, score: number, date: string }[] = [];
+
+    querySnapshot.forEach((doc) => {
+      let data = doc.data();
+      let date: Date = data['Date'].toDate();
+      let newDate: string = this.getDate(date);
+
+        scores.push({
+          userEmail: data['userEmail'],
+          score: data['Score'],
+          date: newDate
+        });
+    });
+
+    return scores;
+  }
+
+  getDate(date: any): string {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+
+    return `${day}/${month}/${year} ${hour}:${minute}`
+  }
+
+  GetGameCode(gameString:string):number {
+    switch (gameString) {
+      case 'Mayor o menor':
+        return 2;
+      case 'Typeracer':
+        return 4;
+      case 'Trivia':
+        return 3;
+      case 'Ahorcado':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
 }
