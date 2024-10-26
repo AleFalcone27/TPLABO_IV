fc5b102ea12fd37d0d10964cb998b0bff0624eda
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-10-26 19:54:57 -0300

    Now admin users can watch survey results

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { ChatComponent } from '../chat/chat.component';
 import { HomecardComponent } from '../homecard/homecard.component';
+import { OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ChatComponent, HomeComponent, HomecardComponent],
+  imports: [ChatComponent, HomeComponent, HomecardComponent,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isAdmin!: boolean;
+  isAdmin$: Observable<boolean>;
 
+  constructor(private authService: AuthService) {
+    this.isAdmin$ = this.authService.adminStatus$;
+    this.isAdmin = this.authService.getAdminStatus();
+  }
+
+  ngOnInit(): void {
+    if (this.authService.isLoggedIn() && !this.authService.getAdminStatus()) {
+      this.checkAdminStatus();
+    }
+  }
+
+  async checkAdminStatus() {
+    this.isAdmin = await this.authService.isAdmin();
+  }
 }
