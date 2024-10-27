c983e08012b898739680d0b81613afe64d356cd7
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-10-26 23:32:49 -0300

    Added IsAdmin guard

---
+++ src/app/guards/admin.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+import { inject } from '@angular/core';
+
+export const adminGuard: CanActivateFn = async (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  try {
+    const isAdmin = await authService.isAdmin();
+    
+    if (isAdmin) {
+      return true;
+    } else {
+      router.navigate(['/home']);
+      return false;
+    }
+  } catch (error) {
+    console.error('Error en adminGuard:', error);
+    router.navigate(['/home']);
+    return false;
+  }
+};
