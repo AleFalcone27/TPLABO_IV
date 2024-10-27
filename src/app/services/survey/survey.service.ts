eebf07729f34fd93f53e1ea4f4741ee3dbcaac6c
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-10-26 23:41:18 -0300

    Added getSurveys and other formatting methods

--- src/app/services/survey/survey.service.ts
+++ src/app/services/survey/survey.service.ts
@@ -1,5 +1,5 @@
 import { Injectable,} from '@angular/core';
-import { Firestore, collection, addDoc, Timestamp } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, Timestamp, query, getDocs, orderBy } from '@angular/fire/firestore';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable({
@@ -25,4 +25,72 @@ export class SurveyService {
       comments: comments
     });
   }
+
+  async getSurveys() {
+    let surveysCollection = collection(this.firestore, 'Surveys');
+    let surveyQuery = query(
+      surveysCollection, 
+      orderBy('Date', 'desc')
+    );
+
+    let querySnapshot = await getDocs(surveyQuery);
+    let surveys: { 
+      comments: string, 
+      favGame: string, 
+      fun: number, 
+      phoneNumber: string, 
+      userEmail: string, 
+      lastname: string,
+      name: string,
+      age: number, 
+      date: string 
+    }[] = [];
+
+    querySnapshot.forEach((doc) => {
+      let data = doc.data();
+      let date: Date = data['Date'].toDate();
+      let newDate: string = this.getDate(date);
+      let stringGame:string = this.GameCodeToString(data['favGame'])
+      surveys.push({
+        date: newDate,    
+        userEmail: data['userEmail'],
+        age: data['age'],
+        comments: data['comments'],
+        name: data['name'],
+        lastname: data['lastName'],
+        favGame: stringGame,
+        fun: data['fun'],
+        phoneNumber: data['phoneNumber']
+      });
+    });
+
+    console.log(surveys)
+    return surveys;
+}
+
+  getDate(date: any): string {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+
+    return `${day}/${month}/${year} ${hour}:${minute}`
+  }
+
+  GameCodeToString(gameCode:number):string {
+    switch (gameCode) {
+      case 1:
+        return 'Mayor o menor';
+      case 4:
+        return 'Typeracer';
+      case 3:
+        return 'Trivia';
+      case 2:
+        return 'Ahorcado';
+      default:
+        return 'Otro';
+    }
+  }
+
 }
