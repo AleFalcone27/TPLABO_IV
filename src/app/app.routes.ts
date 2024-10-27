a6a5829e4057e74771d3148bd685b36bd1a68cbf
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-10-27 14:37:46 -0300

    Added LazyLoading to some of the components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,69 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
-import { WhoAmIComponent } from './components/who-am-i/who-am-i.component';
-import { CardgameComponent } from './components/cardgame/cardgame.component';
-import { TriviagameComponent } from './components/triviagame/triviagame.component';
-import { HangmangameComponent } from './components/hangmangame/hangmangame.component';
-import { TyperacerComponent } from './components/typeracergame/typeracergame.component';
-import { SurveyComponent } from './components/survey/survey.component';
-import { TableComponent } from './components/table/table.component';
+
 import { TableSurveyComponent } from './components/table-survey/table-survey.component';
 import { adminGuard } from './guards/admin.guard';
 
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch:'full' },
+
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'aboutus', component: WhoAmIComponent },
-    { path: 'juegos/mayoromenor', component: CardgameComponent },
-    { path: 'juegos/trivia', component: TriviagameComponent },
-    { path: 'juegos/ahorcado', component: HangmangameComponent },
-    { path: 'juegos/typeracer', component: TyperacerComponent },
-    { path: 'juegos/survey', component: SurveyComponent },
-    { path: 'juegos/table', component: TableComponent },
-    { path : 'juegos/survey/resultados', component: TableSurveyComponent, canActivate: [adminGuard]}
+
+    {
+        path: 'home',
+        loadComponent: () => import('./components/home/home.component')
+            .then(m => m.HomeComponent)
+    },
+
+    {
+        path: 'aboutus',
+        loadComponent: () => import('./components/who-am-i/who-am-i.component')
+            .then(m => m.WhoAmIComponent)
+    },
+
+    {
+        path: 'juegos/mayoromenor',
+        loadComponent: () => import('./components/cardgame/cardgame.component')
+            .then(m => m.CardgameComponent)
+    },
+    {
+        path: 'juegos/ahorcado',
+        loadComponent: () => import('./components/hangmangame/hangmangame.component')
+            .then(m => m.HangmangameComponent)
+    },
+
+    {
+        path: 'juegos/trivia',
+        loadComponent: () => import('./components/triviagame/triviagame.component')
+            .then(m => m.TriviagameComponent)
+    },
+
+    {
+        path: 'juegos/typeracer',
+        loadComponent: () => import('./components/typeracergame/typeracergame.component')
+            .then(m => m.TyperacerComponent)
+    },
+
+    {
+        path: 'juegos/survey',
+        loadComponent: () => import('./components/survey/survey.component')
+            .then(m => m.SurveyComponent)
+    },
+
+    {
+        path: 'juegos/table',
+        loadComponent: () => import('./components/table/table.component')
+            .then(m => m.TableComponent)
+    },
+
+    {
+        path: 'juegos/survey/resultados',
+        loadComponent: () => import('./components/table-survey/table-survey.component')
+            .then(m => m.TableSurveyComponent),
+        canActivate: [adminGuard]
+    },
+
 ];
