e7b32ee43dd40fd4b950eba1740cf9e59610bb8c
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-10-29 15:13:35 -0300

    Added isLoggedIn Guard to games

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-
-import { TableSurveyComponent } from './components/table-survey/table-survey.component';
-import { adminGuard } from './guards/admin.guard';
+import { isLoggedInGuard } from './guards/isLoggedIn/is-logged-in.guard';
+import { adminGuard } from './guards/admin/admin.guard';
 
 
 export const routes: Routes = [
@@ -27,36 +26,42 @@ export const routes: Routes = [
     {
         path: 'juegos/mayoromenor',
         loadComponent: () => import('./components/cardgame/cardgame.component')
-            .then(m => m.CardgameComponent)
+            .then(m => m.CardgameComponent),
+            canActivate: [isLoggedInGuard]
     },
     {
         path: 'juegos/ahorcado',
         loadComponent: () => import('./components/hangmangame/hangmangame.component')
-            .then(m => m.HangmangameComponent)
+            .then(m => m.HangmangameComponent),
+            canActivate: [isLoggedInGuard]
     },
 
     {
         path: 'juegos/trivia',
         loadComponent: () => import('./components/triviagame/triviagame.component')
-            .then(m => m.TriviagameComponent)
+            .then(m => m.TriviagameComponent),
+            canActivate: [isLoggedInGuard],
     },
 
     {
         path: 'juegos/typeracer',
         loadComponent: () => import('./components/typeracergame/typeracergame.component')
-            .then(m => m.TyperacerComponent)
+            .then(m => m.TyperacerComponent),
+            canActivate: [isLoggedInGuard]
     },
 
     {
         path: 'juegos/survey',
         loadComponent: () => import('./components/survey/survey.component')
-            .then(m => m.SurveyComponent)
+            .then(m => m.SurveyComponent),
+            canActivate: [isLoggedInGuard]
     },
 
     {
         path: 'juegos/table',
         loadComponent: () => import('./components/table/table.component')
-            .then(m => m.TableComponent)
+            .then(m => m.TableComponent),
+            canActivate: [isLoggedInGuard]
     },
 
     {
